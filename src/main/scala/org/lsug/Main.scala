ef6baf6c8ac00b810f6c6e82824059833cbedc4f
Rustem Suniev <rustem.suniev@gmail.com>
2011-03-02 01:04:41 +0000

    Another implementation

--- src/main/scala/org/lsug/Main.scala
+++ src/main/scala/org/lsug/Main.scala
@@ -10,16 +10,25 @@ import Scalaz._
 
 object Main {
   def main(args: Array[String]) {
-    val st = genSeq(6,2).take(200)
+    val st = genSeq(numSides = 2,numDie = 2).take(200)
     val ls = groupAndSort(st)
     ls.foreach {
       case (a, b) => println(a + ":" + "X" * b.size)
     }
   }
 
+  /*def genSeq(numSides:Int,numDie:Int): Stream[Int] = {
+    def sum() = (for(_ <- 1 to numDie) yield diceRoll(numSides)) reduceLeft {_+_}
+    Stream.continually(sum)
+    //List.fill(10) (List.fill(3)(rnd(6)).sum)
+  }*/
+
   def genSeq(numSides:Int,numDie:Int): Stream[Int] = {
-     Stream.continually(diceRoll(numSides))
-     //TODO execute function according to numDie's
+    //Grouped returns an Interator???
+    Stream.continually(rnd(numSides)).grouped(numDie).map(_.sum).toStream
+    //how this can be of some help?
+    //what if something went wrong with a dice
+    //http://apocalisp.wordpress.com/2010/10/17/scalaz-tutorial-enumeration-based-io-with-iteratees/
   }
 
   val rnd:Int => Int = (n:Int) => util.Random.nextInt(n)
